0fb3887915027023bfd24fd447795c71286aa14c
lansi951 <eoz754@naver.com>
2014-01-15 00:38:19 +0900

    코드 정리

--- forum.admin.controller.php
+++ forum.admin.controller.php
@@ -28,29 +28,41 @@
             unset($args->forum_name);
 
             // Set parameters default value
-            if($args->except_notice!='Y') $args->except_notice = 'N';
-            if($args->use_anonymous!='Y') $args->use_anonymous= 'N';
-            if($args->allow_anonymous_search!='Y') $args->allow_anonymous_search= 'N';
-            if($args->consultation!='Y') $args->consultation = 'N';
-            if(!in_array($args->order_target,$this->order_target)) $args->order_target = 'list_order';
-            if(!in_array($args->order_type,array('asc','desc'))) $args->order_type = 'asc';
+            if($args->except_notice!='Y')
+                $args->except_notice = 'N';
+            if($args->use_anonymous!='Y')
+                $args->use_anonymous= 'N';
+            if($args->allow_anonymous_search!='Y')
+                $args->allow_anonymous_search= 'N';
+            if($args->consultation!='Y')
+                $args->consultation = 'N';
+            if(!in_array($args->order_target,$this->order_target))
+                $args->order_target = 'list_order';
+            if(!in_array($args->order_type,array('asc','desc')))
+                $args->order_type = 'asc';
 
             // Make sure you are in the right module
-            if($args->module_srl) {
+            if($args->module_srl)
+            {
                 $module_info = $oModuleModel->getModuleInfoByModuleSrl($args->module_srl);
-                if($module_info->module_srl != $args->module_srl) unset($args->module_srl);
+                if($module_info->module_srl != $args->module_srl)
+                    unset($args->module_srl);
             }
 
             // insert/update depending on the value of module_srl
-            if(!$args->module_srl) {
+            if(!$args->module_srl)
+            {
                 $output = $oModuleController->insertModule($args);
                 $msg_code = 'success_registed';
-            } else {
+            }
+            else
+            {
                 $output = $oModuleController->updateModule($args);
                 $msg_code = 'success_updated';
             }
 
-            if(!$output->toBool()) return $output;
+            if(!$output->toBool())
+                return $output;
 
             $this->add('page',Context::get('page'));
             $this->add('act',$args->action);
@@ -67,7 +79,8 @@
             // deletes module with specified module_srl
             $oModuleController = &getController('module');
             $output = $oModuleController->deleteModule($module_srl);
-            if(!$output->toBool()) return $output;
+            if(!$output->toBool())
+                return $output;
 
             $this->add('module','forum');
             $this->add('page',Context::get('page'));
@@ -79,14 +92,18 @@
          **/
         function procForumAdminInsertListConfig() {
             $module_srl = Context::get('module_srl');
-            $list = explode(',',Context::get('list'));
-            if(!count($list)) return new Object(-1, 'msg_invalid_request');
+            $list = explode(',', Context::get('list'));
+            if(!count($list))
+                return new Object(-1, 'msg_invalid_request');
 
             $list_arr = array();
-            foreach($list as $val) {
+            foreach($list as $val)
+            {
                 $val = trim($val);
-                if(!$val) continue;
-                if(substr($val,0,10)=='extra_vars') $val = substr($val,10);
+                if(!$val)
+                    continue;
+                if(substr($val,0,10) == 'extra_vars')
+                    $val = substr($val,10);
                 $list_arr[] = $val;
             }
 
